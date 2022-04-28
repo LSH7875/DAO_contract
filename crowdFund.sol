23d454fa8e89da64c547ba1a05bee34fe011abd6
LSH7875 <tmdgml7875@gmail.com>
2022-04-29 00:48:15 +0900

    crowdfund

--- crowdFund.sol
+++ crowdFund.sol
@@ -26,6 +26,48 @@ contract CrowdFund {
     ) public {
         beneficiary = ifSuccessfulSendTo;
         fundingGoal = fundingGoalInTehres * 1 ether;
-        deal
+        deadline = now +  durationInMinutes * 1 minutes;
+        price = etherCostOfEachToken * 1 ether;
+        tokenReward = token(addressOfTokenUsedAsReward);
+    }
+
+    function () external payable {
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+        balanceOf[msg.sender] += amount;
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
+        emit FundTransfer(msg.sender, amount, true);
+    }
+
+    modifier afterDeadline() { if (now >= deadline) _; }
+
+    function checkGoalReached() external afterDeadline {
+        if(amountRaised >= fundingGoal) {
+            fundingGoalReached = true;
+            emit GoalReached(beneficiary, amountRaised);
+        }
+        crowdsaleClosed = true;
+    }
+
+    function safeWithdrawal () external afterDeadline {
+        if(!fundingGoalReached){
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount> 0){
+                if(msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+        if(fundingGoalReached && beneficiary == msg.sender) {
+            if(beneficiary.send(amountRaised)) {
+                emit FundTransfer(beneficiay, amountRaised, false);
+            } else {
+                fundingGoalReached = false;
+            }
+        }
     }
 }
